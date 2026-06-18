4b5f827b630f4b25684ce8a9ab61065f7049133f
lollipop999 <106931888+lolipop999@users.noreply.github.com>
2026-06-18 10:26:16 -0400

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        front = 0
+        back = len(s) - 1
+        s = s.lower()
+        while front < back:
+            if not self.isAlnum(s[front]):
+                front += 1
+                continue
+            if not self.isAlnum(s[back]):
+                back -= 1
+                continue
+            if s[front] == s[back]:
+                front += 1
+                back -= 1
+            else:
+                return False
+        return True
+    
+    def isAlnum(self, c):
+        if (ord('A') <= ord(c) <= ord('Z')) or (ord('0') <= ord(c) <= ord('9') or (ord('a') <= ord(c) <= ord('z'))):
+            return True
