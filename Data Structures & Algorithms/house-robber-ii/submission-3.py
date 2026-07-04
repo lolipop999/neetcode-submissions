51b6e4f6cebff27886314827f702bce4e8bd0e87
lollipop999 <106931888+lolipop999@users.noreply.github.com>
2026-07-04 10:47:26 -0400

    Add: house-robber-ii - submission-3

---
+++ Data Structures & Algorithms/house-robber-ii/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+
+        nums1 = nums[:-1]
+        nums2 = nums[1:]
+        
+        def maxRob(nums):
+            hashmap = {}
+            skip = 0
+            prev = 0
+            for i in range(len(nums)):
+                if i >= 2:
+                    skip = hashmap[i-2]
+                if i >= 1:
+                    prev = hashmap[i-1]
+                hashmap[i] = max(nums[i] + skip, prev)
+            return hashmap[len(nums)-1]
+        return max(maxRob(nums1), maxRob(nums2))
+        
