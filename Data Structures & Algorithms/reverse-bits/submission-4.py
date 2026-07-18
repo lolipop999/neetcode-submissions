8c3fa2ed3acc4f8cbac4c61850f87f66fc0b9137
lollipop999 <106931888+lolipop999@users.noreply.github.com>
2026-07-18 13:41:30 -0400

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        # reversing the bits
+        res = 0
+        for i in range(31, -1, -1):
+            if n & 1 == 1: # if last number is 1
+                res += 1 << i
+            n = n >> 1 # shift digits over
+        return res
